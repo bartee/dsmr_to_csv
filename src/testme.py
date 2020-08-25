d5a4649787507bd8e628136689253cdf869409ab
Bart Stroeken <bart.stroeken@gmail.com>
2020-08-25 08:05:17 +0100

    feat: write output to csv

--- src/testme.py
+++ src/testme.py
@@ -1,14 +1,41 @@
+import csv
 from dsmr_parser import telegram_specifications, obis_references
 from dsmr_parser.clients import SerialReader, SERIAL_SETTINGS_V4
 from dsmr_parser.parsers import TelegramParser
 
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
 serial_reader = SerialReader(
-    device='/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A13KK398-if00-port0',
+    device=os.getenv('DEVICE_ADDRESS'),
     serial_settings=SERIAL_SETTINGS_V4,
     telegram_specification=telegram_specifications.V4
 )
 
-headers = {'datetime', 'tariff', 'electricity_used_total', 'electricity_delivered_total', 'gas_reading'}
+def store_data(dataset):
+    datestr  = dataset[0].split('-')[0]
+    fname = os.path.abspath("{0}/{1}.csv".format(os.getenv('OUTPUT_DIR'), datestr))
+
+    headers = False
+    if not os.path.isfile(fname):
+        headers = {'datetime', 'tariff', 'electricity_used_total', 'electricity_delivered_total', 'gas_reading'}
+
+    # writing to csv file  
+    with open(fname, 'a') as csvfile:  
+        # creating a csv writer object  
+        csvwriter = csv.writer(csvfile)  
+        if headers:
+            # writing the fields  
+            print('Printing the headers')
+            csvwriter.writerow(headers)        
+        # writing the data rows  
+        csvwriter.writerow(dataset)
+
+
+from pprint import pprint
+
 
 # Todo:
 # - Put try/catch around this
@@ -17,7 +44,7 @@ headers = {'datetime', 'tariff', 'electricity_used_total', 'electricity_delivere
 # - If day changes, create a new CSV
 
 for telegram in serial_reader.read():
-    message_datetime = telegram[obis_references.P1_MESSAGE_TIMESTAMP].value.strftime('%Y%M%d-%H:%I:%S')
+    message_datetime = telegram[obis_references.P1_MESSAGE_TIMESTAMP].value.strftime('%Y%m%d-%H:%I:%S')
     
     # Find out the active tariff
     active_tariff_str = telegram[obis_references.ELECTRICITY_ACTIVE_TARIFF]
@@ -28,6 +55,6 @@ for telegram in serial_reader.read():
 
     gas_reading = telegram[obis_references.HOURLY_GAS_METER_READING].value
     resultset = [message_datetime, active_tariff, str(electricity_used_total), str(electricity_delivered_total), str(gas_reading)]
-
+    store_data(resultset)
     pprint(resultset)
 
